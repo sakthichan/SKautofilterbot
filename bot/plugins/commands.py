8da6c50e9efd8791256d118b603d411ff20174d8
sakthichan <85820819+sakthichan@users.noreply.github.com>
2021-06-15 14:30:34 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = f"{file_name} \n @CC_links_SK",
+                caption = f"{'file_name'} \n @CinemaCity_Chat",
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
